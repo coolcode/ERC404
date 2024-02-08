4313505c8ede020c77c9faf94304c4ea32d603ac
coolcode <coolcode@live.com>
2024-02-08 22:03:02 +0800

    🥑 erc404 & pandora test

--- test/Pandora.t.sol
+++ test/Pandora.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import { Test } from "forge-std/Test.sol";
+import { ERC404 } from "src/ERC404.sol";
 import { Pandora } from "src/Pandora.sol";
 
 contract PandoraTest is Test {
@@ -29,30 +30,53 @@ contract PandoraTest is Test {
     function testTransferHalf() public {
         vm.prank(owner);
         token.transfer(alice, 0.5e18);
-        assertEq(token.balanceOf(alice), 0.5e18, "Alice's balance");
+        assertEq(token.balanceOf(alice), 0.5e18, "Alice's balance should be 0.5");
+        assertEq(token.minted(), 0, "The number of NFT should be 0");
     }
 
     function testTransferOne() public {
         vm.prank(owner);
         token.transfer(alice, 1e18);
-        assertEq(token.balanceOf(alice), 1e18, "Alice's balance");
-        assertEq(token.minted(), 1, "The number of NFT is 1");
+        assertEq(token.balanceOf(alice), 1e18, "Alice's balance should be 1");
+        assertEq(token.minted(), 1, "The number of NFT should be 1");
         assertEq(token.ownerOf(1), alice, "The owner of #1 NFT should be Alice");
     }
 
     function testTransferOneHalf() public {
         vm.prank(owner);
         token.transfer(alice, 1.5e18);
-        assertEq(token.balanceOf(alice), 1.5e18, "Alice's balance");
-        assertEq(token.minted(), 1, "The number of NFT is 1");
+        assertEq(token.balanceOf(alice), 1.5e18, "Alice's balance should be 1.5");
+        assertEq(token.minted(), 1, "The number of NFT should be 1");
         assertEq(token.ownerOf(1), alice, "The owner of #1 nft should be Alice");
     }
 
     function testTransferTen() public {
         vm.prank(owner);
         token.transfer(alice, 10e18);
-        assertEq(token.balanceOf(alice), 10e18, "Alice's balance");
-        assertEq(token.minted(), 10, "The number of NFT is 10");
+        assertEq(token.balanceOf(alice), 10e18, "Alice's balance should be 10");
+        assertEq(token.minted(), 10, "The number of NFT should be 10");
         assertEq(token.ownerOf(10), alice, "The owner of #10 nft should be Alice");
+
+        vm.prank(alice);
+        token.transfer(bob, 2e18); // send 2*10^18 PANDORA to Bob, mint 2 new NFTs to Bob and burn Alice's last 2 NFTs
+        assertEq(token.balanceOf(alice), 8e18, "Alice's balance should be 8 after sending 2 to Bob");
+        assertEq(token.balanceOf(bob), 2e18, "Bob's balance");
+        assertEq(token.minted(), 12, "The number of NFT is 10");
+        assertEq(token.ownerOf(12), bob, "The owner of #10 nft should be Bob");
+    }
+
+    function testTransferRevert() public {
+        vm.expectRevert();
+        vm.prank(alice); // Alice has no token
+        token.transfer(bob, 1e18);
+    }
+
+    function testTransferEmit() public {
+        vm.expectEmit(true, true, true, false);
+        emit ERC404.ERC20Transfer(owner, alice, 1e18);
+        emit ERC404.Transfer(owner, alice, 1);
+
+        vm.prank(owner);
+        token.transfer(alice, 1e18);
     }
 }
